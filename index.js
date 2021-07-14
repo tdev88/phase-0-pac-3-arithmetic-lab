b4f1b5426d7d448b3d9d07164b26fb3c7a2eab57
tdev88 <tracy.dev@outlook.com>
2021-07-14 12:44:18 +1000

    update

---
+++ index.js
@@ -0,0 +1,47 @@
+var a, b
+beforeEach(function() {
+  a = Math.floor(Math.random() * 1000)
+  b = Math.floor(Math.random() * 1000)
+})
+function add(a, b) {
+  a+b
+}
+function subtract(a, b) {
+  a-b
+}
+function multiply(a,b) {
+  a*b
+}
+function divide(a,b) {
+  a/b
+}
+function add(a,b) {
+  return a+b;
+}
+function subtract(a,b) {
+  return a-b;
+}
+function multiply(a,b) {
+  return a*b;
+}
+function divide(a,b) {
+  return a/b;
+}
+function increment(n) {
+  return n +=1 ; 
+}
+function decrement(n) {
+  return n -=1 ;
+}
+function makeInt(n) {
+  return parseInt(n,10);
+  return parseInt('0x2328');
+  return parseInt('sldkjflksjf');
+}
+
+
+function preserveDecimal(n) {
+  return parseFloat(n)
+  return parseFloat('2.222');
+  return pparseFloat('sldkjflksjf');
+}
